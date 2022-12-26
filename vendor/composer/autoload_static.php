ef7d39a3021901905e2336d1bbbc05540613978f
Marlon Jaimes <andres_jr19@hotmail.com>
2022-12-25 19:45:44 -0500

    agregando modelo y migracion denomination

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -531,6 +531,7 @@ class ComposerStaticInit6c6618dc4e3e4fdf23e5c6e9eafeb467
         'App\\Http\\Middleware\\TrustProxies' => __DIR__ . '/../..' . '/app/Http/Middleware/TrustProxies.php',
         'App\\Http\\Middleware\\VerifyCsrfToken' => __DIR__ . '/../..' . '/app/Http/Middleware/VerifyCsrfToken.php',
         'App\\Models\\Company' => __DIR__ . '/../..' . '/app/Models/Company.php',
+        'App\\Models\\Denomination' => __DIR__ . '/../..' . '/app/Models/Denomination.php',
         'App\\Models\\User' => __DIR__ . '/../..' . '/app/Models/User.php',
         'App\\Providers\\AppServiceProvider' => __DIR__ . '/../..' . '/app/Providers/AppServiceProvider.php',
         'App\\Providers\\AuthServiceProvider' => __DIR__ . '/../..' . '/app/Providers/AuthServiceProvider.php',
