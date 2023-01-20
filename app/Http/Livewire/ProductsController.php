375e20a10d6b9df13306cb7ca01d45f0f61eacb1
Marlon Jaimes <andres_jr19@hotmail.com>
2023-01-19 22:21:12 -0500

    se ha agregado el metodo editar y actualizar a los productos

--- app/Http/Livewire/ProductsController.php
+++ app/Http/Livewire/ProductsController.php
@@ -97,6 +97,77 @@ class ProductsController extends Component
         $this->resetUI();
         $this->emit('product-added', 'Producto Registrado');
     }
+    public function Edit(Product $product)
+    {
+        $this->selected_id = $product->id;
+        $this->name = $product->name;
+        $this->barcode = $product->barcode;
+        $this->cost = $product->cost;
+        $this->price = $product->price;
+        $this->stock = $product->stock;
+        $this->alerts = $product->alerts;
+        $this->categoryid = $product->category_id;
+        $this->image = null;
+
+        $this->emit('show-modal', 'Show modal');
+    }
+
+
+    public function Update()
+    {
+        $rules = [
+            'name' => "required|min:3|unique:products,name,{$this->selected_id}",
+            'cost' => 'required',
+            'price' => 'required',
+            'stock' => 'required',
+            'alerts' => 'required',
+            'categoryid' => 'required|not_in:Elegir'
+        ];
+        $messages = [
+            'name.required' => 'El nombre del producto es requerido',
+            'name.unique' => 'Ya existe el nombre del producto',
+            'name.min' => 'El nombre del producto debe tener al menos 3 caracteres',
+            'cost.required' => 'El costo es Requerido',
+            'price.required' => 'El precio es Requerido',
+            'stock.required' => 'El stock es Requerido',
+            'alerts.required' => 'El valor minimo de existencias es requerido',
+            'categoryid.not_in' => 'Elige una categoría diferente',
+
+        ];
+
+        $this->validate($rules, $messages);
+
+        $product = Product::find($this->selected_id);
+
+        $product->update([
+            'name' => $this->name,
+            'cost' => $this->cost,
+            'price' => $this->price,
+            'barcode' => $this->barcode,
+            'stock' => $this->stock,
+            'alerts' => $this->alerts,
+            'category_id' => $this->categoryid,
+        ]);
+
+        if ($this->image) {
+            $customFileName = uniqid() . '_.' . $this->image->extension();
+            $this->image->storeAs('public/products', $customFileName);
+            $imageTemp = $product->image; // imagen temporal
+            $product->image = $customFileName;
+
+            $product->save();
+
+            if ($imageTemp != null) {
+                if (file_exists('storage/products/' . $imageTemp)) {
+                    unlink('storage/products/' . $imageTemp);
+                }
+            }
+        }
+
+        $this->resetUI();
+        $this->emit('product-updated', 'Producto Actualizado');
+    }
+
     public function resetUI()
     {
     }
