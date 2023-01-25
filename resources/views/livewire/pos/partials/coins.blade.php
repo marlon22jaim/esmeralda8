f5d4d77bd65acc1960cb5951d08a5f8de4e996ed
Marlon Jaimes <andres_jr19@hotmail.com>
2023-01-24 22:15:10 -0500

    modificando la vista de las denominaciones

--- resources/views/livewire/pos/partials/coins.blade.php
+++ resources/views/livewire/pos/partials/coins.blade.php
@@ -5,9 +5,11 @@
             <div class="container">
                 <div class="row">
                     @foreach ($denominations as $d)
-                        <div class="col-sm mt-2">
-                            <button wire:click.prevent="ACash({{ $d->value }})" class="btn btn-dark btn-block den">
-                                {{ $d->value > 0 ? '$' . number_format($d->value, 2, '.', '') : 'Exacto' }}
+                        <div class="col-sm-6 col-md-6 mt-2 text-center">
+                            <button wire:click.prevent="ACash({{ $d->value }})" class="btn btn-dark btn-block den"
+                                style="width: 125px">
+                                {{-- {{ $d->value > 0 ? '$' . number_format($d->value, 2, '.', '') : 'Exacto' }} --}}
+                                {{ $d->value > 0 ? '$' . number_format($d->value, 2) : 'Exacto' }}
                             </button>
                         </div>
                     @endforeach
@@ -16,14 +18,16 @@
             <div class="connect-sorting-content mt-4">
                 <div class="card simple-title-task ui-sortable-handle">
                     <div class="card-body">
-                        <div class="input-group input-group-md mb-3">
+                        <div class="input-group input-group-md mb-3"
+                            style=" display: flex;align-items: center;justify-content: center;">
                             <div class="input-group-prepend">
                                 <span class="input-group-text input-gp hideonsm"
                                     style="background: #3b3f5c;color:white">
                                     Efectivo
                                 </span>
                             </div>
-                            <input type="number" id="cash" wire:model="efectivo" wire:keydown.enter="saveSale"
+                            <input style="margin: 5px 5px; height: 35px;border-radius: 2%" type="number"
+                                id="cash" wire:model="efectivo" wire:keydown.enter="saveSale"
                                 class="form-control text-center" value="{{ $efectivo }}">
                             <div class="input-group-append">
                                 <span wire:click="$set('efectivo',0)" style="background: #3b3f5c;color:white"
