320201649a4a8fd3006e8eac5c05ae10510296fa
Marlon Jaimes <andres_jr19@hotmail.com>
2023-01-25 22:03:27 -0500

    importando la clase Cart y creando la funcion ACash

--- app/Http/Livewire/PosController.php
+++ app/Http/Livewire/PosController.php
@@ -3,15 +3,42 @@
 namespace App\Http\Livewire;
 
 use App\Models\Denomination;
+use Darryldecode\Cart\Facades\CartFacade as Cart;
 use Livewire\Component;
 
 class PosController extends Component
 {
-    public $total = 10, $cart = [], $itemsQuantity = 2, $denominations = [], $efectivo, $change;
+    public $total, $itemsQuantity, $efectivo, $change;
+
+    public function mout()
+    {
+        $this->efectivo = 0;
+        $this->change = 0;
+        $this->total = Cart::getTotal();
+        $this->itemsQuantity = Cart::getTotalQuantity();
+    }
+
+
     public function render()
     {
-        $this->denominations = Denomination::all();
         session(['title' => 'Ventas']);
-        return view('livewire.pos.component')->extends('layouts.theme.app')->section('content');
+        return view('livewire.pos.component', [
+            'denominations' => Denomination::orderBy('value', 'desc')->get(),
+            'cart' => Cart::getContent()->sortBy('name')
+        ])
+            ->extends('layouts.theme.app')
+            ->section('content');
     }
+    public function ACash($value)
+    {
+        $this->efectivo += ($value == 0 ? $this->total : $value);
+        $this->change = ($this->efectivo - $this->total);
+    }
+
+    protected $listeners = [
+        'scan-code' => 'ScanCode',
+        'removeItem' => 'removeItem',
+        'crearCart' => 'clearCart',
+        'saveSale' => 'saveSale',
+    ];
 }
