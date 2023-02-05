a3a7baae1cd81cd79d4405d50f0c5d46ce1c1f69
Marlon Jaimes <andres_jr19@hotmail.com>
2023-02-05 01:39:10 -0500

    creando logica para el modulo de reportes

--- app/Http/Livewire/ReportsController.php
+++ app/Http/Livewire/ReportsController.php
@@ -3,11 +3,59 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\User;
+use App\Models\Sale;
+use App\Models\SaleDetails;
+use Carbon\Carbon;
 
 class ReportsController extends Component
 {
+    public $componentName, $data, $details, $sumDetails, $countDetails, $reportType, $userId, $dateFrom, $dateTo, $saleId;
+
+    public function mount()
+    {
+        $this->componentName = "Reportes de Ventas";
+        $this->data = [];
+        $this->details = [];
+        $this->sumDetails = 0;
+        $this->countDetails = 0;
+        $this->reportType = 0;
+        $this->userId = 0;
+        $this->saleId = 0;
+    }
     public function render()
     {
-        return view('livewire.reports.component');
+        session(['title' => 'Reportes']);
+        $this->SalesByDate();
+        return view('livewire.reports.component', [
+            'users' => User::orderBy('name', 'asc')->get()
+        ])->extends('layouts.theme.app')
+            ->section('content');
+    }
+    public function SalesByDate()
+    {
+        if ($this->reportType == 0) {
+            $from = Carbon::parse(Carbon::now())->format('Y-m-d') . ' 00:00:00';
+            $to = Carbon::parse(Carbon::now())->format('Y-m-d') . ' 23:59:59';
+        } else {
+            $from = Carbon::parse($this->dateFrom)->format('Y-m-d') . ' 00:00:00';
+            $to = Carbon::parse($this->dateTo)->format('Y-m-d') . ' 23:59:59';
+        }
+        if ($this->reportType == 1 && ($this->dateFrom == '' || $this->dateTo == '')) {
+            return;
+        }
+
+        if ($this->userId == 0) {
+            $this->data = Sale::join('users as u', 'u.id', 'sales.user_id')
+                ->select('sales.*', 'u.name as user')
+                ->whereBetween('sales.created_at', [$from, $to])
+                ->get();
+        } else {
+            $this->data = Sale::join('users as u', 'u.id', 'sales.user_id')
+                ->select('sales.*', 'u.name as user')
+                ->whereBetween('sales.created_at', [$from, $to])
+                ->where('user_id', $this->userId)
+                ->get();
+        }
     }
 }
