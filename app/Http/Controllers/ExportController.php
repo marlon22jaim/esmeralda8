1b18df5dc71f9f761743ee1e61092cde217cbdb5
Marlon Jaimes <andres_jr19@hotmail.com>
2023-02-05 19:02:28 -0500

    exportar reportes a pdf part2

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -7,11 +7,38 @@ use Barryvdh\DomPDF\Facade as PDF;
 use Carbon\Carbon;
 use App\Models\Sale;
 use App\Models\SaleDetails;
+use App\Models\User;
 use Maatwebsite\Excel\Facades\Excel;
 
 class ExportController extends Controller
 {
     public function reportPDF($userId, $reportType, $dateFrom = null, $dateTo = null)
     {
+        $data = [];
+        if ($this->reportType == 0) {
+            $from = Carbon::parse(Carbon::now())->format('Y-m-d') . ' 00:00:00';
+            $to = Carbon::parse(Carbon::now())->format('Y-m-d') . ' 23:59:59';
+        } else {
+            $from = Carbon::parse($this->dateFrom)->format('Y-m-d') . ' 00:00:00';
+            $to = Carbon::parse($this->dateTo)->format('Y-m-d') . ' 23:59:59';
+        }
+
+        if ($this->userId == 0) {
+            $data = Sale::join('users as u', 'u.id', 'sales.user_id')
+                ->select('sales.*', 'u.name as user')
+                ->whereBetween('sales.created_at', [$from, $to])
+                ->get();
+        } else {
+            $data = Sale::join('users as u', 'u.id', 'sales.user_id')
+                ->select('sales.*', 'u.name as user')
+                ->whereBetween('sales.created_at', [$from, $to])
+                ->where('user_id', $this->userId)
+                ->get();
+        }
+        $user = $userId == 0 ? 'Todos' : User::find($userId)->name;
+        $pdf = PDF::loadView('pdf.reporte', compact('data', 'reportType', 'user', 'dateFrom', 'dateTo'));
+
+        return $pdf->stream('salesReport.pdf');
+        // return $pdf->download('salesReport.pdf');
     }
 }
