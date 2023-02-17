10727a90d3a5c66f84cccb68bb82caff472f8a65
Marlon Jaimes <andres_jr19@hotmail.com>
2023-02-16 22:19:30 -0500

    programacion top5 part2

--- app/Http/Livewire/Dash.php
+++ app/Http/Livewire/Dash.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire;
 
 use App\Models\SaleDetails;
 use Livewire\Component;
+use DB;
 
 class Dash extends Component
 {
@@ -24,7 +25,17 @@ class Dash extends Component
         # code...
         $this->top5Data = SaleDetails::join('products as p', 'sale_details.product_id', 'p.id')
             ->select(
-                DB::raw("p.name as product', 'sum(sale_details.quantity *sale_details.price) as total")
-            )->whereYear("sale_details.created_at", $this->year);
+                DB::raw("p.name as product', sum(sale_details.quantity * sale_details.price) as total")
+            )->whereYear("sale_details.created_at", $this->year)
+            ->groupBy('p.name')
+            ->orderBy(DB::raw("sum(sale_details.quantity * sale_details.price)"), 'desc')
+            ->get()->take(5)->toArray();
+
+        $countDif = (5 - count($this->top5Data));
+        if ($countDif > 0) {
+            for ($i = 1; $i <= $countDif; $i++) {
+                array_push($this->top5Data, ["product" => '-', "total" => 0]);
+            }
+        }
     }
 }
