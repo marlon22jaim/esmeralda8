f9ecd3f6c29162b7220b35e1e54bfa049ad87bc3
Marlon Jaimes <andres_jr19@hotmail.com>
2023-02-16 23:33:12 -0500

    programacion metodo ventas mensuales

--- app/Http/Livewire/Dash.php
+++ app/Http/Livewire/Dash.php
@@ -10,7 +10,7 @@ use DB;
 
 class Dash extends Component
 {
-    public $year, $salesBuMonth_Data = [], $top5Data = [], $weekSales_Data = [];
+    public $year, $salesByMonth_Data = [], $top5Data = [], $weekSales_Data = [];
 
     public function mount()
     {
@@ -19,6 +19,9 @@ class Dash extends Component
     }
     public function render()
     {
+        $this->getTop5();
+        $this->getSalesMonth();
+        $this->getWeekSales();
         session(['title' => 'Home']);
         return view('livewire.dashboard.component')->extends('layouts.theme.app')->section('content');
     }
@@ -27,7 +30,7 @@ class Dash extends Component
         # code...
         $this->top5Data = SaleDetails::join('products as p', 'sale_details.product_id', 'p.id')
             ->select(
-                DB::raw("p.name as product', sum(sale_details.quantity * sale_details.price) as total")
+                DB::raw("p.name as product, sum(sale_details.quantity * sale_details.price) as total")
             )->whereYear("sale_details.created_at", $this->year)
             ->groupBy('p.name')
             ->orderBy(DB::raw("sum(sale_details.quantity * sale_details.price)"), 'desc')
@@ -39,6 +42,7 @@ class Dash extends Component
                 array_push($this->top5Data, ["product" => '-', "total" => 0]);
             }
         }
+        // dd($this->top5Data);
     }
 
     public function getWeekSales()
@@ -56,5 +60,35 @@ class Dash extends Component
 
             array_push($this->weekSales_Data, $wsale);
         }
+        //dd($this->weekSales_Data);
+    }
+    public function getSalesMonth()
+    {
+        $salesByMonth = DB::select(
+            DB::raw("SELECT COALESCE(total, 0) AS total
+                FROM (
+                    SELECT 'january' AS month
+                    UNION SELECT 'february' AS month
+                    UNION SELECT 'march' AS month
+                    UNION SELECT 'april' AS month
+                    UNION SELECT 'may' AS month
+                    UNION SELECT 'june' AS month
+                    UNION SELECT 'july' AS month
+                    UNION SELECT 'august' AS month
+                    UNION SELECT 'september' AS month
+                    UNION SELECT 'october' AS month
+                    UNION SELECT 'november' AS month
+                    UNION SELECT 'december' AS month
+                ) m
+                LEFT JOIN (SELECT MONTHNAME(created_at) as MONTH, COUNT(*) as orders, SUM(total) as total
+                FROM sales WHERE year(created_at)= $this->year
+                GROUP BY MONTHNAME(created_at),MONTH(created_at)
+                ORDER BY MONTH(created_at)) c ON m.MONTH = c.MONTH;")
+        );
+
+        foreach ($salesByMonth as $sale) {
+            array_push($this->salesByMonth_Data, $sale->total);
+        }
+       // dd($this->salesByMonth_Data);
     }
 }
