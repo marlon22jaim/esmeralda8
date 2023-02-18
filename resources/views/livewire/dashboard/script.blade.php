f9d0871341689c3099178b8d4c5a95254480cb67
Marlon Jaimes <andres_jr19@hotmail.com>
2023-02-17 21:38:13 -0500

    agregando grafico de ventas por mes

--- resources/views/livewire/dashboard/script.blade.php
+++ resources/views/livewire/dashboard/script.blade.php
@@ -67,7 +67,7 @@
             dataLabels: {
                 enabled: true,
                 formatter: function(val) {
-                    return '$' + val
+                    return '$' + parseFloat(val).toFixed(2)
                 },
                 offsetY: -5,
                 style: {
@@ -90,6 +90,104 @@
 
         let chart2 = new ApexCharts(document.querySelector("#areaChart"), options2);
         chart2.render();
+
+        // ------------------------------------------------------//
+        //                    VENTAS MENSUALES                   //
+        // ------------------------------------------------------//
+
+        let options3 = {
+            series: [{
+                name: 'Ventas del mes',
+                data: @this.salesByMonth_Data
+            }],
+            chart: {
+                height: 350,
+                type: 'bar',
+            },
+            plotOptions: {
+                bar: {
+                    borderRadius: 10,
+                    dataLabels: {
+                        position: 'top', // top, center, bottom
+                    },
+                }
+            },
+            dataLabels: {
+                enabled: true,
+                formatter: function(val) {
+                    return "$" + parseFloat(val).toFixed(2);
+                },
+                offsetY: -20,
+                style: {
+                    fontSize: '12px',
+                    colors: ["#304758"]
+                }
+            },
+
+            xaxis: {
+                categories: ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov",
+                    "Dic"
+                ],
+                position: 'top',
+                axisBorder: {
+                    show: false
+                },
+                axisTicks: {
+                    show: false
+                },
+                crosshairs: {
+                    fill: {
+                        type: 'gradient',
+                        gradient: {
+                            colorFrom: '#D8E3F0',
+                            colorTo: '#BED1E6',
+                            stops: [0, 100],
+                            opacityFrom: 0.4,
+                            opacityTo: 0.5,
+                        }
+                    }
+                },
+                tooltip: {
+                    enabled: true,
+                }
+            },
+            yaxis: {
+                axisBorder: {
+                    show: false
+                },
+                axisTicks: {
+                    show: false,
+                },
+                labels: {
+                    show: false,
+                    formatter: function(val) {
+                        return "$" + parseFloat(val).toFixed(2);
+                    }
+                }
+
+            },
+            title: {
+                text: totalSalesYear(),
+                floating: true,
+                offsetY: 330,
+                align: 'center',
+                style: {
+                    color: '#444'
+                }
+            }
+        };
+
+        let chart3 = new ApexCharts(document.querySelector("#chartMonth"), options3);
+        chart3.render();
+
+        function totalSalesYear() {
+            let total = 0
+            @this.salesByMonth_Data.forEach(item => {
+                total += parseFloat(item)
+            })
+            return "Total: $" + total.toFixed(2)
+        }
+
 
     })
 </script>
