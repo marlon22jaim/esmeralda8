071256b6e4c9573cf7babcfab865b67cdc6ef502
Marlon Jaimes <andres_jr19@hotmail.com>
2023-02-19 22:00:08 -0500

    haciendo que el buscador de productos funcione de manera global para agregar productos

--- app/Http/Livewire/SearchController.php
+++ app/Http/Livewire/SearchController.php
@@ -2,11 +2,45 @@
 
 namespace App\Http\Livewire;
 
+use App\Traits\CartTrait;
 use Livewire\Component;
+use Illuminate\Support\Str;
+use Darryldecode\Cart\Facades\CartFacade as Cart;
+
 
 class SearchController extends Component
 {
+    use CartTrait;
     public $search;
+    public $currentPath;
+
+    public function mount()
+    {
+        $this->currentPath = url()->current();
+    }
+
+    protected $listeners = [
+        'scan-code' => 'ScanCode',
+    ];
+
+    public function ScanCode($barcode)
+    {
+        $routeName = Str::afterLast($this->currentPath, '/');
+        if ($routeName != "pos") {
+            $this->ScanearCode($barcode);
+
+            redirect()->to('/pos');
+        }
+    }
+    public function getQuantity($productId)
+    {
+        $exist = Cart::get($productId);
+        if ($exist) {
+            return $exist->quantity;
+        } else {
+            return 0;
+        }
+    }
     public function render()
     {
         return view('livewire.search');
