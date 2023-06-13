317790a8991222cacbe7b9845641960a159b31dc
Marlon Jaimes <andres_jr19@hotmail.com>
2023-06-13 06:47:16 -0500

    retoques

--- resources/views/layouts/theme/header.blade.php
+++ resources/views/layouts/theme/header.blade.php
@@ -9,48 +9,44 @@
 
     <livewire:search-controller>
 
-    <nav class="header-nav ms-auto">
-        <ul class="d-flex align-items-center">
-            <li class="nav-item d-block d-lg-none">
-                <a class="nav-link nav-icon search-bar-toggle" href="#">
-                    <i class="bi bi-search"></i>
-                </a>
-            </li>
+        <nav class="header-nav ms-auto">
+            <ul class="d-flex align-items-center">
+                <li class="nav-item d-block d-lg-none">
+                    <a class="nav-link nav-icon search-bar-toggle" href="#">
+                        <i class="bi bi-search"></i>
+                    </a>
+                </li>
 
-            <li class="nav-item dropdown pe-3">
-                <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-                    <img src="assets/img/logo_Persona.svg" alt="Profile" class="rounded-circle" />
-                    <span class="d-none d-md-block dropdown-toggle ps-2">Marlon</span>
-                </a>
-                <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
-                    <li class="dropdown-header">
-                        <h6>Marlon</h6>
-                        <span>Web Designer</span>
-                    </li>
-                    <li>
-                        <hr class="dropdown-divider" />
-                    </li>
-                    <li>
-                        <a class="dropdown-item d-flex align-items-center" href="#">
-                            <i class="bi bi-person"></i>
-                            <span>Mi Perfíl</span>
-                        </a>
-                    </li>
-                    <li>
-                        <hr class="dropdown-divider" />
-                    </li>
-                    <li>
-                        <a onclick="event.preventDefault(); document.getElementById('logout-form').submit()"
-                            class="dropdown-item d-flex align-items-center" href="{{ route('logout') }}">
-                            <i class="bi bi-box-arrow-right"></i>
-                            <span>Cerrar sesión</span>
-                        </a>
-                        <form action="{{ route('logout') }}" method="POST" id="logout-form">
-                            @csrf
-                        </form>
-                    </li>
-                </ul>
-            </li>
-        </ul>
-    </nav>
+                <li class="nav-item dropdown pe-3">
+                    <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#"
+                        data-bs-toggle="dropdown">
+                        <img src="assets/img/logo_Persona.svg" alt="Profile" class="rounded-circle" />
+                        <span class="d-none d-md-block dropdown-toggle ps-2">{{ Auth::user()->name }}</span>
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
+                        <li class="dropdown-header">
+                            <h6>{{ Auth::user()->name }}</h6>
+                            <span>Sistema la Esmeralda</span>
+                        </li>
+                        <li>
+                            <hr class="dropdown-divider" />
+                        </li>
+
+                       <li>
+                            <hr class="dropdown-divider" />
+                        </li>
+                        <li>
+                            <a onclick="event.preventDefault(); document.getElementById('logout-form').submit()"
+                                class="dropdown-item d-flex align-items-center" href="{{ route('logout') }}">
+                                <i class="bi bi-box-arrow-right"></i>
+                                <span>Cerrar sesión</span>
+                            </a>
+                            <form action="{{ route('logout') }}" method="POST" id="logout-form">
+                                @csrf
+                            </form>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
 </header>
