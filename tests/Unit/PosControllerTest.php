b758e4c6054a6da6c5bb3f9d655ac0e0f77ca4b7
Marlon Jaimes <andres_jr19@hotmail.com>
2023-06-13 06:00:46 -0500

    test PosControllerTest

---
+++ tests/Unit/PosControllerTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Tests\Unit;
+
+
+use App\Http\Livewire\PosController;
+use Darryldecode\Cart\Facades\CartFacade as Cart;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class PosControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testMout()
+    {
+        // Crear instancia del controlador
+        $controller = new PosController();
+
+        // Llamar al método mout
+        $controller->mout();
+
+        // Verificar que las propiedades se han actualizado correctamente
+        $this->assertEquals(0, $controller->efectivo);
+        $this->assertEquals(0, $controller->change);
+        // Realiza las aserciones adicionales que consideres necesarias
+    }
+
+    public function testRender()
+    {
+        // Crear instancia del controlador
+        $controller = new PosController();
+
+        // Llamar al método render
+        $view = $controller->render();
+
+        // Verificar que la vista se ha generado correctamente
+        $this->assertNotEmpty($view);
+        // Realiza las aserciones adicionales que consideres necesarias
+    }
+
+    public function testSaveSale()
+    {
+        // Crear instancia del controlador
+        $controller = new PosController();
+
+        // Simular los datos necesarios para la prueba
+        $controller->total = 100;
+        $controller->itemsQuantity = 2;
+        $controller->efectivo = 150;
+        $controller->change = 50;
+
+        // Ejecutar el método saveSale()
+        $controller->saveSale();
+
+        // Verificar que el carrito se haya limpiado después de guardar la venta
+        $this->assertTrue(Cart::isEmpty());
+    }
+}
